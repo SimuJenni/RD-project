821cc393b71598acd178d6b1d5b843e01640d3c8
Simon Jenni <simujenni@students.unibe.ch>
2016-04-04 16:43:39 +0200

    Added another Spectrum plot
    Spectrum for ROI with maximal FFT power added

--- code/Frequency Analysis/spectrumPlots.m
+++ code/Frequency Analysis/spectrumPlots.m
@@ -16,12 +16,28 @@ filePath = [saveDir fileName '_Avg_Spect.png'];
 saveas(gcf,filePath);
 close(fig)
 
+
+%% Spectrum of ROI with maximal power
+fig = figure('visible', 'off');
+[maxv, ~] = max(power);
+[~, idx] = max(maxv(:));
+[row,col] = ind2sub([size(power,2), size(power,3)], idx);
+plot(f(1:round(end/2)),power(1:round(end/2),row,col))
+xlabel('Frequency (Hz)')
+ylabel('Power')
+title('{\bf CBF Spectrum of ROI with Maximum FFT-Power}')
+
+% Save
+filePath = [saveDir fileName '_Max_Spect.png'];
+saveas(gcf,filePath);
+close(fig)
+
 %% Distribution of dominant frequencies
 fig = figure('visible', 'off');
 histogram(domFreqs(domFreqs<46),100);
 xlabel('Frequency (Hz)')
 ylabel('Count')
-title('{\bf Distribution of dominant frequencies}')
+title('{\bf Distribution of Dominant Frequencies per ROI}')
 
 % Save
 filePath = [saveDir fileName '_Freq_Dist.png'];
