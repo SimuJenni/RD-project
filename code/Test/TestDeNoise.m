44309dc3a92d86aee095832eea8f9d63abeee6bb
Simon Jenni <simujenni@students.unibe.ch>
2016-04-17 00:09:09 +0200

    Added a function which performs 3d smoothing for noise-removal.
    Either using 3d gaussian kernel or 3d-gaussian + median.
    Also added test script to compare different methods.

---
+++ code/Test/TestDeNoise.m
@@ -0,0 +1,42 @@
+setup
+
+%% Test different parameters for denoising
+
+dims = [3, 3, 3];           % filter dimensions
+sigmas = [0.3, 0.3, 0.5];   % parameters for gaussians
+denoise2 = @(x) filter3d(x, dims, sigmas);
+
+dims = [3, 3, 3];           % filter dimensions
+sigmas = [0.3, 0.3, 0.5];   % parameters for gaussians
+denoise3 = @(x) filter3d(x, dims, sigmas, 'median');
+
+
+% Get a list of all .mat files
+FileList = dir([extractedDir '*.mat']); 
+numFiles = length(FileList); 
+for idx = 1:numFiles
+    fileName = FileList(idx).name;
+    
+    % Load the file
+    file = load([extractedDir fileName]);
+    [~, videoId, ~] = fileparts([extractedDir fileName]);
+    
+    % Denoise and save 
+    data = denoise1(file.data);
+    save([extractedDir videoId '_dn1.mat'], 'data');
+    
+    data = denoise2(file.data);
+    save([extractedDir videoId '_dn2.mat'], 'data');
+    
+    data = denoise3(file.data);
+    save([extractedDir videoId '_dn3.mat'], 'data');
+    
+    data = denoise4(file.data);
+    save([extractedDir videoId '_dn4.mat'], 'data');
+
+end
+
+
+
+
+
---
+++ code/filter3d.m
@@ -0,0 +1,44 @@
+function [ data ] = filter3d( data, dims, sigmas, type )
+%FILTER3D Filters the provided 3d-array data with a 3d-gaussian kernel with
+%dimensions specified in dims and gaussians specidied by sigmas.
+% Input:
+%   data - 3D array (width x height x frames)
+%   dims - (hx, hy, hz) of filter dimensions
+%   sigmas - (sx, sy, sz) parameters for gaussian filters
+%   type - 'gaussian' or 'median' defining type of third-dim filter
+% Output:
+%   data - filtered input
+
+%% Check inputs
+
+if length(dims) ~= 3 || length(sigmas) ~= 3
+    error('Input ERROR: Expected arguments to be of length 3')
+end
+
+if nargin < 4
+    type = 'gaussian';
+elseif ~strcmp(type, 'gaussian') && ~strcmp(type, 'median')
+    error('Input ERROR: Expected type to be "gaussian" or "median"')
+end
+
+%% Filtering
+
+% Third dimension either gaussian or median
+if strcmp(type, 'gaussian')
+    g_z = fspecial('gaussian', [1, dims(3)], sigmas(3));
+    g_z = reshape(g_z,[1 1 dims(3)]);
+    data = convn(data, g_z, 'same');
+else
+    data = medfilt1(data,3,[],3);
+end
+
+% Filtering is seperable! Perform three 1D convolutions -> Faster
+g_x = fspecial('gaussian', [1, dims(1)], sigmas(1));
+g_y = fspecial('gaussian', [dims(2), 1], sigmas(2));
+
+% Filter first two dimensions with gaussians
+data = convn(data, g_x, 'same');
+data = convn(data, g_y, 'same');
+
+end
+
