009b625ee7552a76c6f3403f0d7bacf2dc8e027a
Simon Jenni <simujenni@students.unibe.ch>
2016-04-18 23:30:22 +0200

    Implemented activity mask

---
+++ code/Activity Analysis/activityMask.m
@@ -0,0 +1,11 @@
+function [ mask ] = activityMask( activity, lowQ, highQ )
+%ACTIVITYMASK Generates a mask indicating which pixels of the activity are
+%within a band defined by the given quantiles of the activity distribution.
+
+lowB = quantile(activity(:), lowQ);
+highB = quantile(activity(:), highQ);
+
+mask = (activity > lowB) & (activity < highB);
+
+end
+
