75198f9b1b0be0be5f16156f8475e50591fd5195
Simon Jenni <simujenni@students.unibe.ch>
2016-06-08 23:34:55 +0200

    Improved shape extraction
    Used function fitting using smoothing splines

--- code/probableShapeFromData.m
+++ code/probableShapeFromData.m
@@ -1,36 +1,41 @@
 function [ shape ] = probableShapeFromData( dominantFreqs, phase, data,...
-    fs )
+    activity, fs )
 %PROBABLESHAPEFROMDATA Computes the probable beating pattern from the
 %results of the FFT and the underlying data.
 %   Detailed explanation goes here
 
 % Get frequency region with most ROIs
-[N,edges] = histcounts(dominantFreqs(dominantFreqs>4),90);
+[N,edges] = histcounts(dominantFreqs(dominantFreqs>4),200);
 [v, I] = max(N);
 freqRange = edges(I:I+1);
 
+% Activity-band
+minActivity = quantile(activity(:), 0.9);
+activityBand = activity>minActivity;
+
 % Length of region to extract
-l = ceil(1.5*fs/freqRange(1));    
+l = ceil(2*fs/freqRange(1));    
 
 % Compute phase-shift
 B = dominantFreqs*2*pi;
 phase(phase>0)=phase(phase>0)-2*pi;     % Wrap phases around making all <0
 phaseShift = -phase./B;
-phaseShift = uint8(round(phaseShift*fs));   % Convert into frame-indices
 
-% Shift the data so that all are in same phase
-shiftData = zeros(size(data,1), size(data,2), l);
-for i=1:size(data,1)
-    for j=1:size(data,2)
-        shiftData(i, j, :) = data(i, j, phaseShift(i, j)+1:phaseShift(i, j)+l);
-    end
-end
-shiftData = reshape(shiftData,[],l);
+% The time axis
+X = repmat((0:l-1)/fs, numel(phaseShift), 1) - repmat(phaseShift(:), 1, l);
+% Corresponding data
+Y = double(reshape(data(:,:,1:l), [], l));
 
-% Compute shape as mean shape of curves within frequency range of interest
+% Frequencies and data of interrest
 freqBand = dominantFreqs>freqRange(1) & dominantFreqs<freqRange(2);
-shape = mean(shiftData(freqBand,:));
-plot(shape);
+valid = freqBand & activityBand;
+X = X(valid(:),:);
+Y = Y(valid(:),:);
+
+% Fit using splines and plot
+shape = fit(X(:), Y(:),  'smoothingspline', 'SmoothingParam', 0.4);
+plot(shape,X(:), Y(:));
+
 
 end
 
