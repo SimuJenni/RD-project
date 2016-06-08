b7630619eb1691724a6df0a9829748b8fa6d975a
Simon Jenni <simujenni@students.unibe.ch>
2016-06-08 23:34:14 +0200

    Added probable shape extraction to demo

--- code/demo.m
+++ code/demo.m
@@ -13,9 +13,9 @@ filterDims = [3, 3, 3];     % diemensions of 3d gaussian kernel
 sigmas = [0.3, 0.3, 0.5];   % parameters for gaussians
 denoise = @(x) filter3d(x, filterDims, sigmas);
 
-roiSize = [3, 4];
+% roiSize = [3, 4];
 % roiSize = [6, 8];
-% roiSize = 1;
+ roiSize = 1;
 batchAnalyseFolder( extractedDir, fs, roiSize, resultsDir, denoise , 'fft' )
 
 %delete(POOL);
@@ -23,16 +23,18 @@ batchAnalyseFolder( extractedDir, fs, roiSize, resultsDir, denoise , 'fft' )
 % Launch the GUI for interpretation of the results
 gui
 
-% %% Demo using WT
-% load([extractedDir 'Cylia_beating_movie.mat'])
-% [ power, f, domFreqs ] = WTAnalysis( data, fs );
-%
-% %% Demo using FFT
-% load([extractedDir 'Cylia_beating_movie.mat'])
-% [ power, f, domFreqs, domPhase ] = performFFT( data, fs );
-% 
-% %% Demo activity image
-% activity = fftPowerActivity( power );
-% activity = computeEntropy( data );
-% 
-% generateActivityImage( activity, 'Entropy' );
+%% Demo using WT
+load([extractedDir 'Cylia_beating_movie.mat'])
+[ power, f, domFreqs ] = WTAnalysis( data, fs );
+
+%% Demo using FFT
+load([extractedDir 'Cylia_beating_movie.mat'])
+[ power, f, domFreqs, domPhase ] = performFFT( data, fs );
+
+%% Demo activity image
+activity = fftPowerActivity( power );
+activity = computeEntropy( data );
+generateActivityImage( activity, 'Entropy' );
+
+%% Demo probable shape extraction
+shape = probableShapeFromData( dominantFreqs, dominantPhase, data, activity, fs );
