6014e7e21074655bc770cf320f7aea2c63929fc8
Simon Jenni <simujenni@students.unibe.ch>
2016-06-18 15:09:24 +0200

    Improved probable shape computation to use wighted LLS

--- code/probableShapeFromData.m
+++ code/probableShapeFromData.m
@@ -16,13 +16,15 @@ function [ shape ] = probableShapeFromData( dominantFreqs, phase, data,...
 % See also PERFORMFFT, COMPUTEENTROPY, ACTIVITYFROMPOWER.
 
 % Get frequency region with most ROIs
-[N,edges] = histcounts(dominantFreqs(dominantFreqs>4),200);
+[N,edges] = histcounts(dominantFreqs(dominantFreqs>4),400);
 [v, I] = max(N);
 freqRange = edges(I:I+1);
 
 % Activity-band
-minActivity = quantile(activity(:), 0.9);
-activityBand = activity>minActivity;
+minActivity = quantile(activity(:), 0.95);
+maxActivity = quantile(activity(:), 1);
+
+activityBand = activity>=minActivity & activity<=maxActivity;
 
 % Length of region to extract
 l = ceil(2*fs/freqRange(1));    
@@ -43,10 +45,45 @@ valid = freqBand & activityBand;
 X = X(valid(:),:);
 Y = Y(valid(:),:);
 
-% Fit using splines and plot
-shape = fit(X(:), Y(:),  'smoothingspline', 'SmoothingParam', 0.4);
-plot(shape,X(:), Y(:));
-
+% Fit using weighted linear least squares with 1st degree polynomial model
+scatter(X(:),Y(:));
+x = linspace(min(X(:)),max(X(:)));
+shape = mylowess([X(:),Y(:)],x,0.2);
+line(x, shape, 'color', 'r', 'linestyle', '-', 'linewidth', 2.5);
 
 end
 
+function ys=mylowess(xy,xs,span)
+%MYLOWESS Lowess smoothing, preserving x values
+%   YS=MYLOWESS(XY,XS) returns the smoothed version of the x/y data in the
+%   two-column matrix XY, but evaluates the smooth at XS and returns the
+%   smoothed values in YS.  Any values outside the range of XY are taken to
+%   be equal to the closest values.
+
+if nargin<3 || isempty(span)
+    span = .3;
+end
+
+% Sort and get smoothed version of xy data
+xy = sortrows(xy);
+x1 = xy(:,1);
+y1 = xy(:,2);
+ys1 = smooth(x1,y1,span,'loess');
+
+% Remove repeats so we can interpolate
+t = diff(x1)==0;
+x1(t)=[]; ys1(t) = [];
+
+% Interpolate to evaluate this at the xs values
+ys = interp1(x1,ys1,xs,'linear',NaN);
+
+% Some of the original points may have x values outside the range of the
+% resampled data.  Those are now NaN because we could not interpolate them.
+% Replace NaN by the closest smoothed value.  This amounts to extending the
+% smooth curve using a horizontal line.
+if any(isnan(ys))
+    ys(xs<x1(1)) = ys1(1);
+    ys(xs>x1(end)) = ys1(end);
+end
+
+end
