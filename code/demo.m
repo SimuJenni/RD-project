b7d64c1827df26b07f08977297cb371cdcdf4801
Laurent Hayez <zeta-1@users.noreply.github.com>
2016-06-21 13:30:41 +0200

    Reset demo script to original one

--- code/demo.m
+++ code/demo.m
@@ -13,28 +13,28 @@ filterDims = [3, 3, 3];     % diemensions of 3d gaussian kernel
 sigmas = [0.3, 0.3, 0.5];   % parameters for gaussians
 denoise = @(x) filter3d(x, filterDims, sigmas);
 
-roiSize = [3, 4];
+% roiSize = [3, 4];
 % roiSize = [6, 8];
-% roiSize = 1;
-batchAnalyseFolder( extractedDir, fs, roiSize, resultsDir, denoise , 'wt', 'morl')
+roiSize = 1;
+batchAnalyseFolder( extractedDir, fs, roiSize, resultsDir, denoise , 'fft')
 
 %delete(POOL);
 
 % Launch the GUI for interpretation of the results
-%gui
+gui
 
 %% Demo using WT
-%load([extractedDir 'Cylia_beating_movie.mat'])
-%[ power, f, domFreqs ] = WTAnalysis( data, fs );
+load([extractedDir 'Cylia_beating_movie.mat'])
+[ power, f, domFreqs ] = WTAnalysis( data, fs );
 
 %% Demo using FFT
-%load([extractedDir 'Cylia_beating_movie.mat'])
-%[ power, f, domFreqs, domPhase ] = performFFT( data, fs );
+load([extractedDir 'Cylia_beating_movie.mat'])
+[ power, f, domFreqs, domPhase ] = performFFT( data, fs );
 
 %% Demo activity image
-%activity = activityFromPower( power );
+activity = activityFromPower( power );
 % activity = computeEntropy( data );
-%generateActivityImage( activity, 'Entropy' );
+generateActivityImage( activity, 'Entropy' );
 
 %% Demo probable shape extraction
-%shape = probableShapeFromData( dominantFreqs, dominantPhase, data, activity, fs );
+shape = probableShapeFromData( dominantFreqs, dominantPhase, data, activity, fs );
